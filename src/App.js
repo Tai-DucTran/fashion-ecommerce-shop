9332761f6e20159e9a85715159efc4e4557e135f
Tai-DucTran <taitran.deve@gmail.com>
2023-12-25 23:07:44 +0700

    Components: create category-section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import CategorySectionComponent from "./components/category/category-section.component";
+import categories from "./constants/constants";
+
 const App = () => {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <div className="categories-container">
+        {categories.map((category) => (
+          <CategorySectionComponent title={category.title} key={category.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
