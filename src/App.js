5f7fdd4cc81b0ba7ae1d7f1dac0d8e6344d2d0e4
Tai-DucTran <taitran.deve@gmail.com>
2023-12-26 00:02:14 +0700

    Packages: installed and used SCSS library

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import CategorySectionComponent from "./components/category/category-section.component";
 import categories from "./constants/constants";
 
+import "./App.styles.scss";
+
 const App = () => {
   return (
-    <div className="App">
-      <div className="categories-container">
-        {categories.map((category) => (
-          <CategorySectionComponent title={category.title} key={category.id} />
-        ))}
-      </div>
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategorySectionComponent title={category.title} key={category.id} />
+      ))}
     </div>
   );
 };
