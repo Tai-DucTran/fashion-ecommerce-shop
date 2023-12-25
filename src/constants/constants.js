105a1434b0f55dae0b97dd6a0a519727c90c539f
Tai-DucTran <taitran.deve@gmail.com>
2023-12-25 23:08:28 +0700

    Constants: created constants.js

---
+++ src/constants/constants.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    id: 1,
+    title: "HATS",
+  },
+  {
+    id: 2,
+    title: "JACKETS",
+  },
+  {
+    id: 3,
+    title: "SNEAKERS",
+  },
+  {
+    id: 4,
+    title: "WOMEN'S",
+  },
+  {
+    id: 5,
+    title: "MEN'S",
+  },
+];
+
+export default categories;
