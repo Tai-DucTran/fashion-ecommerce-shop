03a3a05665997f6e528976b93debd068c9a98ea2
Tai-DucTran <taitran.deve@gmail.com>
2024-01-14 21:37:38 +0700

    routes: create navigation-bar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import NavigationBar from "./routes/navigation-bar/navigation-bar.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
-  return Directory();
+  return (
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
