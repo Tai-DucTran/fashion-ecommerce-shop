690467d78a626608249a5dd5be22d64a6dc343bf
Tai-DucTran <taitran.deve@gmail.com>
2024-01-14 22:38:49 +0700

    routes: create logo container and modified index.scss

--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,12 +1,15 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation-bar.styles.scss";
+
 const NavigationBar = () => {
   return (
     <Fragment>
       <div className="navigation-bar">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
