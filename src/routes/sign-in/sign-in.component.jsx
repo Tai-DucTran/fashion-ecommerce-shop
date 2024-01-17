9e24d0769646aa59cd76beb457564c799efe9c5c
Tai-DucTran <taitran.deve@gmail.com>
2024-01-17 16:09:28 +0700

    SignUp: created signInWithGoogleRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,36 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    const verifyRedirectResult = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocFromAuth(response.user);
+      }
+    };
+    verifyRedirectResult();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
