dcd8948df54333831579695b64d01ea847604c93
Tai-DucTran <taitran.deve@gmail.com>
2024-01-23 12:02:40 +0700

    components: added style fỏ sign-up-form

--- src/routes/sign-up-form/sign-up-form.components.jsx
+++ src/routes/sign-up-form/sign-up-form.components.jsx
@@ -7,6 +7,8 @@ import {
   createUserDocFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
+import './sign-up-form.style.scss';
+
 const defaultForm = {
   displayName: '',
   email: '',
@@ -58,8 +60,9 @@ const SiginUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
