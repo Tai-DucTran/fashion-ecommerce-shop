95a076c7a17145f458a56f3af29b67c708be5973
Tai-DucTran <taitran.deve@gmail.com>
2024-02-04 20:37:53 +0700

    fix: cart-icon onCartClickHandler has a wrong argu

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,9 +4,9 @@ import { CartContext } from '../../contexts/cart/cart.context';
 import './cart-icon.style.scss';
 
 const CartIcon = () => {
-  const { isCartDropDown, setIsCartDropDown } = useContext(CartContext);
+  const { isCartOpen, setIsCartDropDown } = useContext(CartContext);
   const onCartClickHandler = () => {
-    setIsCartDropDown(!isCartDropDown);
+    setIsCartDropDown(!isCartOpen);
   };
   return (
     <div className="cart-icon-container" onClick={onCartClickHandler}>
