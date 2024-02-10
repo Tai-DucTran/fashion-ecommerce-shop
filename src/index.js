5b3386c13d334911f31ab7ccb4d57e5a9d478dd9
Tai-DucTran <taitran.deve@gmail.com>
2024-02-10 23:59:34 +0700

    Refactor: changed name from ProductProvider -> CategoriesProvider

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user/user.context';
-import { ProductsProvider } from './contexts/products/products.context';
 import { CartProvider } from './contexts/cart/cart.context';
 
 import './index.scss';
+import { CategoriesProvider } from './contexts/categories/categories.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
