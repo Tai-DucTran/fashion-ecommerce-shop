28c4a6ea5fb49b9c9a297d6ff01342d68e5261e4
Tai-DucTran <taitran.deve@gmail.com>
2024-02-14 12:21:36 +0700

    Styled: refactored authentication component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,14 @@
-import './authentication.style.scss';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
+import { AuthenticationContainer } from './authentication.style';
+
 const Authentication = () => {
   return (
-    <div className="authentication-container">
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
