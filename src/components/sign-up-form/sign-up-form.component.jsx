0e8473ad9d3e22c7989bce4d96b416534b24d33e
Tai-DucTran <taitran.deve@gmail.com>
2024-02-15 14:49:42 +0700

    Styled: migrated sign-up-form with styled-component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -6,7 +6,7 @@ import {
 
 import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
-import './sign-up-form.style.scss';
+import { SignUpContainer } from './sign-up-form.style';
 
 const defaultForm = {
   displayName: '',
@@ -59,7 +59,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <SignUpContainer>
       <h2>Don't have an account</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -101,7 +101,7 @@ const SignUpForm = () => {
 
         <Button chidren={'Sign Up'}></Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
