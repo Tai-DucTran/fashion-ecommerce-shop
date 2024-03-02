6d1d618da34724e0f8f9ee60f7430e2113fd8b8e
Tai-DucTran <taitran.deve@gmail.com>
2024-03-02 22:49:19 +0700

    Store: applied redux-persist

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './store/store';
+import { store, persistedStore } from './store/store';
 
 import './index.scss';
 
@@ -14,9 +15,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
