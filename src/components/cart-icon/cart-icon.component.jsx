0be6c41d51966cc927995f6defb7dcadee9d3120
Tai-DucTran <taitran.deve@gmail.com>
2024-03-04 00:44:32 +0700

    Components: replaced SignOut with Profile

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,6 +5,8 @@ import {
   selectCartCount,
 } from '../../store/cart/cart.selectors';
 import { setIsCartOpen } from '../../store/cart/cart.actions';
+import { selectIsProfileCardOpen } from '../../store/user/user.selector';
+import { setIsProfileCartOpen } from '../../store/user/user.action';
 
 import { ShoppingIcon, CartIconContainer, ItemCount } from './cart-icon.style';
 
@@ -13,7 +15,12 @@ const CartIcon = () => {
   const isCartOpen = useSelector(selectIsCartOpen);
   const cartCount = useSelector(selectCartCount);
 
+  const isProfileCardOpen = useSelector(selectIsProfileCardOpen);
+
   const onCartClickHandler = () => {
+    if (isProfileCardOpen) {
+      dispatch(setIsProfileCartOpen(!isProfileCardOpen));
+    }
     dispatch(setIsCartOpen(!isCartOpen));
   };
 
--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,14 +1,20 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
-import { selectCurrentUser } from '../../store/user/user.selector';
+
+import {
+  selectCurrentUser,
+  selectIsProfileCardOpen,
+} from '../../store/user/user.selector';
+import { setIsProfileCartOpen } from '../../store/user/user.action';
 import { selectIsCartOpen } from '../../store/cart/cart.selectors';
+import { setIsCartOpen } from '../../store/cart/cart.actions';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import ProfileCardContainer from '../profile-card/profile-card.component';
 
 import {
   NavigationBarContainer,
@@ -18,10 +24,16 @@ import {
 } from './navigation-bar.styles';
 
 const NavigationBar = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
-  const signOutHandle = async () => {
-    await signOutUser();
+  const isProfileCardOpen = useSelector(selectIsProfileCardOpen);
+
+  const onProfileClickHandler = () => {
+    if (isCartOpen) {
+      dispatch(setIsCartOpen(!isCartOpen));
+    }
+    dispatch(setIsProfileCartOpen(!isProfileCardOpen));
   };
 
   return (
@@ -34,12 +46,13 @@ const NavigationBar = () => {
           <NavLink to="/shop">SHOP</NavLink>
           <NavLink to="/contact">CONTACT</NavLink>
           {currentUser ? (
-            <NavLink as="span" onClick={signOutHandle}>
-              SIGN OUT
+            <NavLink as="span" onClick={onProfileClickHandler}>
+              PROFILE
             </NavLink>
           ) : (
             <NavLink to="/auth">SIGN IN</NavLink>
           )}
+          {isProfileCardOpen && <ProfileCardContainer />}
           <CartIcon />
         </NavLinks>
         {isCartOpen && <CartDropdown />}
