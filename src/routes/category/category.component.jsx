d8312f96c010697ad9d787fc77a74fe47dcd3a61
Tai-DucTran <taitran.deve@gmail.com>
2024-03-03 14:50:53 +0700

    Migration: Refactor with Redux-Thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,13 +2,19 @@ import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoadingState,
+} from '../../store/categories/categories.selector';
+
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
 import { CategoryContainer, CategoryTitle } from './category.style';
+import Spinner from '../../components/spinner/spinner.component';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoadingState);
 
   const [products, setProducts] = useState(categoriesMap[category]);
 
@@ -19,12 +25,16 @@ const Category = () => {
   return (
     <>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {products &&
-          products.map(product => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </>
   );
 };
